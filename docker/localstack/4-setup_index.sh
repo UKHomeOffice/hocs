ac4337829b2b2ba7c8bdc58a2bd7472753b494cc
Stuart Whitlam <stuart.whitlam1@digital.homeoffice.gov.uk>
2022-06-15 11:25:09 +0100

    make elastic mapping location generic in setup index script so that it works locally, in drone and docker compose

--- docker/localstack/4-setup_index.sh
+++ docker/localstack/4-setup_index.sh
@@ -5,7 +5,9 @@ export AWS_ACCESS_KEY_ID=UNSET
 export AWS_SECRET_ACCESS_KEY=UNSET
 export AWS_DEFAULT_REGION=eu-west-2
 
-ELASTIC_MAPPING_PATH="/docker-entrypoint-initaws.d/elastic_mapping.json"
+SCRIPT_LOCATION=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+echo $SCRIPT_LOCATION
+ELASTIC_MAPPING_PATH="${SCRIPT_LOCATION}/elastic_mapping.json"
 
 if ! [[ -f "${ELASTIC_MAPPING_PATH}" ]]
 then
