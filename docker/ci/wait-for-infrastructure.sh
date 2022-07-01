827701861bc5c65694019754774dc41bc27e2512
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-07-01 14:21:04 +0100

    Spike infra test

--- docker/ci/wait-for-infrastructure.sh
+++ docker/ci/wait-for-infrastructure.sh
@@ -4,6 +4,6 @@ set -euo
 until curl 'localhost.localstack.cloud:4566/es/eu-west-2/decs/_cat/indices?v' --silent | grep -q "local-case"; do
    sleep 20
    echo "Waiting for Elasticsearch Index to be present..."
-   docker-compose logs -t
+   docker-compose -f ./docker/docker-compose.yml logs -t
    curl 'localhost.localstack.cloud:4566/es/eu-west-2/decs/local-case'
 done
