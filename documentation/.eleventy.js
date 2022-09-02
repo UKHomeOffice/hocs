d44cab824e7c47dc2f1b58fee352278e5aa11129
Robert Deniszczyc <robert.deniszczyc@digital.homeoffice.gov.uk>
2022-09-02 16:15:19 +0100

    Add elevnty - documentation

---
+++ documentation/.eleventy.js
@@ -0,0 +1,20 @@
+const govukEleventyPlugin = require('govuk-eleventy-plugin')
+
+module.exports = function(eleventyConfig) {
+  // Register the plugin
+  eleventyConfig.addPlugin(govukEleventyPlugin, {
+    header: {
+      productName: 'DECS Documentation',
+    }
+  })
+
+  return {
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+    dir: {
+      // Use layouts from the plugin
+      layouts: 'node_modules/govuk-eleventy-plugin/layouts'
+    }
+  }
+};
